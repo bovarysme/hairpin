22f4bd3eec09b097a7022fb7eaa305451253706e
bovarysme <bovarysme@users.noreply.github.com>
2021-01-08 00:14:28 +0100

    Switch to supervisor mode

---
+++ src/csr.zig
@@ -0,0 +1,28 @@
+pub const mstatus_mpp_s = 1 << 11;
+
+pub fn read(comptime name: []const u8) usize {
+    return asm volatile ("csrr %[ret], " ++ name
+        : [ret] "=r" (-> usize)
+    );
+}
+
+pub fn write(comptime name: []const u8, value: usize) void {
+    asm volatile ("csrw " ++ name ++ ", %[value]"
+        :
+        : [value] "r" (value)
+    );
+}
+
+pub fn set(comptime name: []const u8, mask: usize) void {
+    asm volatile ("csrs " ++ name ++ ", %[mask]"
+        :
+        : [mask] "r" (mask)
+    );
+}
+
+pub fn clear(comptime name: []const u8, mask: usize) void {
+    asm volatile ("csrc " ++ name ++ ", %[mask]"
+        :
+        : [mask] "r" (mask)
+    );
+}
--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,4 @@
+const csr = @import("csr.zig");
 const uart = @import("uart.zig").uart;
 
 export const stack_size: u32 = 4096;
@@ -7,14 +8,25 @@ export const stack_size: u32 = 4096;
 export var stack: [stack_size]u8 align(16) = undefined;
 
 export fn start() noreturn {
+    // Set the Machine Previous Privilege to S-mode
+    csr.set("mstatus", csr.mstatus_mpp_s);
+
+    // Set the Machine Exception Program Counter to the address of `main`
+    csr.write("mepc", @ptrToInt(main));
+
+    // Return to `main` in S-mode
+    asm volatile ("mret");
+
+    unreachable;
+}
+
+fn main() noreturn {
     // Initialize the UART
     uart.init();
     const writer = uart.writer();
-    try writer.print("Hello, {}!\n", .{"World"});
+    try writer.print("Hello from {}!\n", .{"S-mode"});
 
     while (true) {
         asm volatile ("wfi");
     }
-
-    unreachable;
 }
